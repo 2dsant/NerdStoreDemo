336228cb2ab3a2bf443a5fbf842aefe19476058a
David Santos <david.santos@finanblue.com.vc>
2025-03-18 13:46:56 -0300

    backup

--- src/NerdStoreDemo.Catalogo.Domain/Events/ProdutoAbaixoEstoqueEvent.cs
+++ src/NerdStoreDemo.Catalogo.Domain/Events/ProdutoAbaixoEstoqueEvent.cs
@@ -6,11 +6,8 @@ public class ProdutoAbaixoEstoqueEvent : DomainEvent
 {
     public int QuantidadeRestante { get; private set; }
 
-    public ProdutoAbaixoEstoqueEvent(Guid aggregateId, int quantidadeRestante) 
-        : base(aggregateId)
+    public ProdutoAbaixoEstoqueEvent(Guid aggregateId, int quantidadeRestante) : base(aggregateId)
     {
         QuantidadeRestante = quantidadeRestante;
     }
-
-
 }
