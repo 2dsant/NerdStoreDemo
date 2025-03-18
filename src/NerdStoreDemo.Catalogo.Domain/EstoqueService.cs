a8dc6d1648d05e351e772e61c90df02b4be6c4a4
David Santos <david.santos@finanblue.com.vc>
2025-03-18 13:37:16 -0300

    backp

--- src/NerdStoreDemo.Catalogo.Domain/EstoqueService.cs
+++ src/NerdStoreDemo.Catalogo.Domain/EstoqueService.cs
@@ -8,8 +8,7 @@ public class EstoqueService : IEstoqueService
     private readonly IProdutoRepository _produtoRepository;
     private readonly IMediatrHandler _bus;
 
-    public EstoqueService(IProdutoRepository produtoRepository, 
-        IMediatrHandler bus)
+    public EstoqueService(IProdutoRepository produtoRepository, IMediatrHandler bus)
     {
         _produtoRepository = produtoRepository;
         _bus = bus;
@@ -18,15 +17,15 @@ public class EstoqueService : IEstoqueService
     public async Task<bool> DebitarEstoque(Guid produtoId, int quantidade)
     {
         var produto = await _produtoRepository.ObterPorId(produtoId);
-        if (produto == null) return false;
+
+        if (produto is null) return false;
 
         if (!produto.PossuiEstoque(quantidade)) return false;
 
         produto.DebitarEstoque(quantidade);
 
         if (produto.QuantidadeEstoque < 10)
-            await _bus.PublicarEvento(new ProdutoAbaixoEstoqueEvent(produtoId, 
-                produto.QuantidadeEstoque));
+            await _bus.PublicarEvento(new ProdutoAbaixoEstoqueEvent(produtoId, produto.QuantidadeEstoque));
 
         _produtoRepository.Atualizar(produto);
 
@@ -36,8 +35,11 @@ public class EstoqueService : IEstoqueService
     public async Task<bool> ReporEstoque(Guid produtoId, int quantidade)
     {
         var produto = await _produtoRepository.ObterPorId(produtoId);
-        if (produto == null) return false;
+
+        if (produto is null) return false;
+
         produto.ReporEstoque(quantidade);
+
         _produtoRepository.Atualizar(produto);
 
         return await _produtoRepository.UnitOfWork.Commit();
