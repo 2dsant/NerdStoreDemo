918d241b61bc75f674a173c9cb9c00332c4f1f79
David Santos <2dsant3@gmail.com>
2025-03-19 22:21:19 -0300

    Adicionado camada de aplicação

---
+++ src/NerdStore.Catalogo.Application/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using NerdStore.Catalogo.Application.ViewModels;
+using NerdStoreDemo.Catalogo.Domain;
+using AutoMapper;
+
+namespace NerdStore.Catalogo.Application.AutoMapper;
+
+public class ViewModelToDomainMappingProfile : Profile
+{
+    public ViewModelToDomainMappingProfile()
+    {
+        // construct using porque a Model utiliza construtor
+
+        CreateMap<ProdutoViewModel, Produto>()
+            .ConstructUsing(p =>
+                new Produto(p.Nome, p.Descricao, p.Ativo,
+                    p.Valor, p.CategoriaId, p.DataCadastro,
+                    p.Imagem, new Dimensoes(p.Altura, p.Largura, p.Profundidade)));
+
+        CreateMap<CategoriaViewModel, Categoria>()
+            .ConstructUsing(c => new Categoria(c.Nome, c.Codigo));
+    }
+}
